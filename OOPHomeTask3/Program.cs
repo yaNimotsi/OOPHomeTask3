e524aaedd3ea3ff88ed99ffd139457033fd02522
Иван Истомин <nimotsiky@gmail.com>
2021-10-21 12:02:32 +0300

    Task1 is complate

--- OOPHomeTask3/Program.cs
+++ OOPHomeTask3/Program.cs
@@ -6,7 +6,10 @@ namespace OOPHomeTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account1 = new BankAccount(100, TypeBankAccount.Deposit);
+            var account2 = new BankAccount(100, TypeBankAccount.Deposit);
+
+            account2.RemoveCashToAccount(account1, 33.3);
         }
     }
 }

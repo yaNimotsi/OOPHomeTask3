2e8735df96671e4cec26eff0b99e7243b9017bf4
Иван Истомин <nimotsiky@gmail.com>
2021-10-31 10:23:41 +0300

    1

--- Lesson4.2/CreatorClasses/Creator.cs
+++ Lesson4.2/CreatorClasses/Creator.cs
@@ -1,9 +1,21 @@
-﻿using Lesson4._2.Interfaces;
+﻿using System.Collections;
+using Lesson4._2.Interfaces;
 
 namespace Lesson4._2.CreatorClasses
 {
     internal abstract class Creator
     {
+        public static Hashtable createdBuildingHashtable = new Hashtable();
+
+        protected Creator()
+        {
+            var building = CreateBuild();
+        }
+
+        private static void AddBuildingToHashTable(IBuilding building)
+        {
+            createdBuildingHashtable.Add(building.BuildingNumber, building);
+        }
 
         public abstract IBuilding CreateBuild();
     }

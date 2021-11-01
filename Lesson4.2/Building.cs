27129198a958baa4c83e4b0102ccdb571fb8b96b
IstominI <IStomin.I.A@Vnipipt.ru>
2021-11-01 14:18:40 +0300

    Apdate building class. Add filed, changed property

--- Lesson4.2/Building.cs
+++ Lesson4.2/Building.cs
@@ -6,16 +6,37 @@ namespace Lesson4._2
     internal class Building : IBuilding
     {
         private static int _buildingNumber;
+        private double _buildingHeight;
+        private int _numberOfFloor;
+        private int _numberApartments;
+        private int _numberEntrances;
+
 
         public int BuildingNumber => _buildingNumber;
         
-        public double BuildingHeight { get; set; }
+        public double BuildingHeight 
+        {
+            get => _buildingHeight;
+            set => _buildingHeight = value;
+        }
 
-        public int NumberOfFloor { get; set; }
+        public int NumberOfFloor
+        {
+            get => _numberOfFloor;
+            set => _numberOfFloor = value;
+        }
 
-        public int NumberApartments { get; set; }
+        public int NumberApartments
+        {
+            get => _numberApartments;
+            set => _numberApartments = value;
+        }
 
-        public int NumberEntrances { get; set; }
+        public int NumberEntrances
+        {
+            get => _numberEntrances;
+            set => _numberEntrances = value;
+        }
 
         private Building()
         {
@@ -24,15 +45,15 @@ namespace Lesson4._2
 
         protected internal  Building(double buildingHeight, int numberOfFloor, int numberApartments, int numberEntrances)
         {
-            BuildingHeight = buildingHeight;
-            NumberOfFloor = numberOfFloor;
-            NumberApartments = numberApartments;
-            NumberEntrances = numberEntrances;
+            _buildingHeight = buildingHeight;
+            _numberOfFloor = numberOfFloor;
+            _numberApartments = numberApartments;
+            _numberEntrances = numberEntrances;
             
-            GenerteBuildingNumber();
+            GenerateBuildingNumber();
         }
 
-        private void GenerteBuildingNumber()
+        private void GenerateBuildingNumber()
         {
             _buildingNumber++;
         }
@@ -40,12 +61,12 @@ namespace Lesson4._2
         /// <summary>
         /// Return the height of the floor
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Height of the floor</returns>
         public double GetHeightFloor()
         {
-            if (BuildingHeight > 0 && NumberOfFloor > 0)
+            if (_buildingHeight > 0 && _numberOfFloor > 0)
             {
-                return BuildingHeight / NumberOfFloor;
+                return _buildingHeight / _numberOfFloor;
             }
             
             return 0;
@@ -54,12 +75,12 @@ namespace Lesson4._2
         /// <summary>
         /// Return number apartments in entrance
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Number of appartments in the Entrance</returns>
         public int GetCountApartmentInEntrance()
         {
-            if (NumberApartments > 0 && NumberEntrances > 0)
+            if (_numberApartments > 0 && _numberEntrances > 0)
             {
-                return NumberApartments / NumberEntrances;
+                return _numberApartments / _numberEntrances;
             }
             
             return 0;
@@ -68,14 +89,14 @@ namespace Lesson4._2
         /// <summary>
         /// Rreturn count apartment in floor
         /// </summary>
-        /// <returns></returns>
+        /// <returns> Number of appartments on the floor</returns>
         public int GetCountApartmentInFloor()
         {
             var apartmentInEntrance = GetCountApartmentInEntrance();
 
             if (apartmentInEntrance > 0)
             {
-                return apartmentInEntrance / NumberOfFloor;
+                return apartmentInEntrance / _numberOfFloor;
             }
 
             return 0;
@@ -93,9 +114,9 @@ namespace Lesson4._2
                 return false;
             }
 
-            Building build = (Building)obj;
+            var build = (Building)obj;
 
-            return this.BuildingNumber == build.BuildingNumber;
+            return _buildingNumber == build.BuildingNumber;
         }
 
 

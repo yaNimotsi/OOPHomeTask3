0dfa94b6e45144a59bbe7169c134e270174bf9a3
IstominI <IStomin.I.A@Vnipipt.ru>
2021-11-09 09:22:57 +0300

    Add overide method ToString(). Add implicit and explicit to float type.

--- Task1/RationalNumber.cs
+++ Task1/RationalNumber.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Task1
 {
@@ -44,6 +45,76 @@ namespace Task1
             double decimalRepresentation2 = number.Numeration / number.Denominator;
 
             return Math.Abs(decimalRepresentation1 - decimalRepresentation2) < 0.001;
+        }
+
+        public override string ToString()
+        {
+            double result = Numeration / Denominator;
+            return new string(Convert.ToString(result));
+        }
+
+        /// <summary>
+        /// Получение НОД от двух чисел
+        /// </summary>
+        /// <param name="newNumeration"> Преобразованный числитель</param>
+        /// <param name="newDenominator"> Преобразлванный знаменатель</param>
+        /// <returns>НОД двух чисел</returns>
+        private static int GCD(int newNumeration, int newDenominator)
+        {
+            if (newNumeration == newDenominator) return newNumeration;
+            if (newNumeration == 1 || newDenominator == 1) return 1;
+            if ((newNumeration % 2 == 0) && (newDenominator % 2 == 0)) return 2 * GCD(newNumeration / 2, newDenominator / 2);
+            if ((newNumeration % 2 == 0) && (newDenominator % 2 != 0)) return GCD(newNumeration / 2, newDenominator);
+            if ((newNumeration % 2 != 0) && (newDenominator % 2 == 0)) return GCD(newNumeration, newDenominator / 2);
+            return GCD(newDenominator, (int)Math.Abs(newNumeration - newDenominator));
+        }
+
+        /// <summary>
+        /// Получение кол-ва знаков после запятой
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>0, если знаков после запятой нет. Иначе кол-во знаков после запятой
+        /// </returns>
+        private static int GetDecimalDigitsCount(double number)
+        {
+            var separator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
+            var str = number.ToString(separator).Split(separator);
+            return str.Length == 2 ? str[1].Length : 0;
+        }
+
+        /// <summary>
+        /// Conver RationalNumber to floatValue
+        /// </summary>
+        /// <param name="rationalNumber"></param>
+        public static implicit operator float (RationalNumber rationalNumber)
+        {
+            return Math.Abs(rationalNumber.Numeration/ rationalNumber.Denominator);
+        }
+
+        /// <summary>
+        /// Conver value from float to RationalNumber
+        /// </summary>
+        /// <param name="val"> Float value to convert</param>
+        public static explicit operator RationalNumber (float val)
+        {
+            var decimalDigitsCount = GetDecimalDigitsCount(val);
+            if (decimalDigitsCount == 0)
+            {
+                return new RationalNumber()
+                {
+                    Numeration = (int)val,
+                    Denominator = 1
+                };
+            }
+
+            var newNumeration = (int)(val * (Math.Pow(10, decimalDigitsCount)));
+            var newDenominator = (int)Math.Pow(10, decimalDigitsCount);
+
+            return new RationalNumber()
+            {
+                Numeration = newNumeration / GCD((int)newNumeration, (int)newDenominator),
+                Denominator = newDenominator / GCD(newNumeration, newDenominator)
+            };
         }
     }
 }

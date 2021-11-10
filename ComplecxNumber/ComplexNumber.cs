68dbfcae9f0ddcd9be2b753bf094164e226cc12b
Иван Истомин <nimotsiky@gmail.com>
2021-11-10 20:55:34 +0300

    Start do complex number

---
+++ ComplecxNumber/ComplexNumber.cs
@@ -0,0 +1,58 @@
+﻿namespace ComplecxNumber
+{
+    class ComplexNumber
+    {
+        private double _realPart;
+        private double _imaginaryPart;
+
+        public double RealPart
+        {
+            get => _realPart; 
+            set => _realPart = value;
+        }
+
+        public double ImaginaryPart
+        {
+            get => _imaginaryPart;
+            set => _imaginaryPart = value;
+        }
+
+        private ComplexNumber()
+        {
+            
+        }
+
+        public ComplexNumber(double realNumber, double imaginaryPart)
+        {
+            _realPart = realNumber;
+            _imaginaryPart = imaginaryPart;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber number1, ComplexNumber number2)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = number1.RealPart + number2.RealPart,
+                ImaginaryPart = number1.ImaginaryPart + number2.ImaginaryPart
+            };
+        }
+
+        public static ComplexNumber operator -(ComplexNumber number1, ComplexNumber number2)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = number1.RealPart - number2.RealPart,
+                ImaginaryPart = number1.ImaginaryPart - number2.ImaginaryPart
+            };
+        }
+
+        public static ComplexNumber operator *(ComplexNumber number1, ComplexNumber number2)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = number1.RealPart * number2.RealPart + (number1.ImaginaryPart * number2.ImaginaryPart)*(-1),
+                ImaginaryPart = number1.ImaginaryPart * number2.RealPart + number1.RealPart * number2.ImaginaryPart
+            };
+        }
+    }
+}

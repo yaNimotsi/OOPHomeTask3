490cda9c57fb07fa4e0a700a59cc423155af918b
IstominI <IStomin.I.A@Vnipipt.ru>
2021-11-10 10:15:50 +0300

    Осталось реализовать %. Как - я хз

--- Task1/RationalNumber.cs
+++ Task1/RationalNumber.cs
@@ -1,43 +1,29 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Task1
 {
-    class RationalNumber
+    internal class RationalNumber
     {
-        private int _numerator;
-        private int _denominator;
-
-        public int Numeration
-        {
-            get => _numerator;
-            set => _numerator = value;
-        }
-
-        public int Denominator
-        {
-            get => _denominator;
-            set => _denominator = value;
-        }
-
         private RationalNumber()
         {
-
         }
 
         public RationalNumber(int numerator, int denominator)
         {
             if (denominator == 0) return;
-            _numerator = numerator;
-            _denominator = denominator;
+            Numeration = numerator;
+            Denominator = denominator;
         }
+
+        public int Numeration { get; set; }
+
+        public int Denominator { get; set; }
 
         public override bool Equals(object? obj)
         {
-            if (obj == null)
-            {
-                return false;
-            }
+            if (obj == null) return false;
 
             var number = (RationalNumber)obj;
 
@@ -54,7 +40,7 @@ namespace Task1
         }
 
         /// <summary>
-        /// Получение НОД от двух чисел
+        ///     Получение НОД от двух чисел
         /// </summary>
         /// <param name="newNumeration"> Преобразованный числитель</param>
         /// <param name="newDenominator"> Преобразлванный знаменатель</param>
@@ -63,17 +49,19 @@ namespace Task1
         {
             if (newNumeration == newDenominator) return newNumeration;
             if (newNumeration == 1 || newDenominator == 1) return 1;
-            if ((newNumeration % 2 == 0) && (newDenominator % 2 == 0)) return 2 * GCD(newNumeration / 2, newDenominator / 2);
-            if ((newNumeration % 2 == 0) && (newDenominator % 2 != 0)) return GCD(newNumeration / 2, newDenominator);
-            if ((newNumeration % 2 != 0) && (newDenominator % 2 == 0)) return GCD(newNumeration, newDenominator / 2);
-            return GCD(newDenominator, (int)Math.Abs(newNumeration - newDenominator));
+            if (newNumeration % 2 == 0 && newDenominator % 2 == 0)
+                return 2 * GCD(newNumeration / 2, newDenominator / 2);
+            if (newNumeration % 2 == 0 && newDenominator % 2 != 0) return GCD(newNumeration / 2, newDenominator);
+            if (newNumeration % 2 != 0 && newDenominator % 2 == 0) return GCD(newNumeration, newDenominator / 2);
+            return GCD(newDenominator, Math.Abs(newNumeration - newDenominator));
         }
 
         /// <summary>
-        /// Получение кол-ва знаков после запятой
+        ///     Получение кол-ва знаков после запятой
         /// </summary>
         /// <param name="number"></param>
-        /// <returns>0, если знаков после запятой нет. Иначе кол-во знаков после запятой
+        /// <returns>
+        ///     0, если знаков после запятой нет. Иначе кол-во знаков после запятой
         /// </returns>
         private static int GetDecimalDigitsCount(double number)
         {
@@ -83,42 +71,40 @@ namespace Task1
         }
 
         /// <summary>
-        /// Convert RationalNumber to floatValue
+        ///     Convert RationalNumber to floatValue
         /// </summary>
         /// <param name="rationalNumber"></param>
-        public static implicit operator float (RationalNumber rationalNumber)
+        public static implicit operator float(RationalNumber rationalNumber)
         {
-            return Math.Abs(rationalNumber.Numeration/ rationalNumber.Denominator);
+            return Math.Abs(rationalNumber.Numeration / rationalNumber.Denominator);
         }
 
         /// <summary>
-        /// Convert value from float to RationalNumber
+        ///     Convert value from float to RationalNumber
         /// </summary>
         /// <param name="val"> Float value to convert</param>
-        public static explicit operator RationalNumber (float val)
+        public static explicit operator RationalNumber(float val)
         {
             var decimalDigitsCount = GetDecimalDigitsCount(val);
             if (decimalDigitsCount == 0)
-            {
-                return new RationalNumber()
+                return new RationalNumber
                 {
                     Numeration = (int)val,
                     Denominator = 1
                 };
-            }
 
-            var newNumeration = (int)(val * (Math.Pow(10, decimalDigitsCount)));
+            var newNumeration = (int)(val * Math.Pow(10, decimalDigitsCount));
             var newDenominator = (int)Math.Pow(10, decimalDigitsCount);
 
-            return new RationalNumber()
+            return new RationalNumber
             {
-                Numeration = newNumeration / GCD((int)newNumeration, (int)newDenominator),
+                Numeration = newNumeration / GCD(newNumeration, newDenominator),
                 Denominator = newDenominator / GCD(newNumeration, newDenominator)
             };
         }
 
         /// <summary>
-        /// Convert RationalNumber to int Value
+        ///     Convert RationalNumber to int Value
         /// </summary>
         /// <param name="rationalNumber"> Rational number to convert</param>
         public static implicit operator int(RationalNumber rationalNumber)
@@ -127,17 +113,120 @@ namespace Task1
         }
 
         /// <summary>
-        /// Convert value from int to RationalNumber
+        ///     Convert value from int to RationalNumber
         /// </summary>
         /// <param name="val"> Int value to convert</param>
         public static explicit operator RationalNumber(int val)
         {
-            return new RationalNumber()
+            return new RationalNumber
             {
-                Numeration = (int)val,
+                Numeration = val,
                 Denominator = 1
             };
         }
 
+        /// <summary>
+        /// addition two rational number
+        /// </summary>
+        /// <param name="rationalNumber1"></param>
+        /// <param name="rationalNumber2"></param>
+        /// <returns> Sum of two rational number</returns>
+        public static RationalNumber operator +(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return new RationalNumber
+            {
+                Numeration = rationalNumber1.Numeration * rationalNumber2.Denominator +
+                             rationalNumber1.Denominator * rationalNumber2.Numeration,
+                Denominator = rationalNumber1.Denominator * rationalNumber2.Denominator
+            };
+        }
+
+        /// <summary>
+        /// subtraction two rational number
+        /// </summary>
+        /// <param name="rationalNumber1"></param>
+        /// <param name="rationalNumber2"></param>
+        /// <returns> difference of two numbers</returns>
+        public static RationalNumber operator -(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return new RationalNumber
+            {
+                Numeration = rationalNumber1.Numeration * rationalNumber2.Denominator -
+                             rationalNumber1.Denominator * rationalNumber2.Numeration,
+                Denominator = rationalNumber1.Denominator * rationalNumber2.Denominator
+            };
+        }
+
+        public static RationalNumber operator ++(RationalNumber rationalNumber)
+        {
+            rationalNumber.Numeration += rationalNumber.Denominator;
+            return rationalNumber;
+        }
+
+        public static RationalNumber operator --(RationalNumber rationalNumber)
+        {
+            rationalNumber.Numeration -= rationalNumber.Denominator;
+            return rationalNumber;
+        }
+
+        public static int operator %(RationalNumber numeration, float denominator)
+        {
+            
+            return 0;
+        }
+
+        /// <summary>
+        /// multiplication two rational number
+        /// </summary>
+        /// <param name="rationalNumber1"></param>
+        /// <param name="rationalNumber2"></param>
+        /// <returns>multiplication of two numbers</returns>
+        public static RationalNumber operator *(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return new RationalNumber
+            {
+                Numeration = rationalNumber1.Numeration * rationalNumber2.Numeration,
+                Denominator = rationalNumber1.Denominator * rationalNumber2.Denominator
+            };
+        }
+
+        /// <summary>
+        /// division of two rational number
+        /// </summary>
+        /// <param name="rationalNumber1"></param>
+        /// <param name="rationalNumber2"></param>
+        /// <returns>division of two numbers</returns>
+        public static RationalNumber operator /(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return new RationalNumber
+            {
+                Numeration = rationalNumber1.Numeration * rationalNumber2.Denominator,
+                Denominator = rationalNumber1.Denominator * rationalNumber2.Numeration
+            };
+        }
+
+        public static bool operator >(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return (float)Math.Abs(rationalNumber1.Numeration / rationalNumber1.Denominator) > 
+                   (float)Math.Abs(rationalNumber2.Numeration / rationalNumber2.Denominator);
+        }
+
+        public static bool operator <(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return (float)Math.Abs(rationalNumber1.Numeration / rationalNumber1.Denominator) <
+                   (float)Math.Abs(rationalNumber2.Numeration / rationalNumber2.Denominator);
+        }
+
+        public static bool operator >=(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return (float)Math.Abs(rationalNumber1.Numeration / rationalNumber1.Denominator) >=
+                   (float)Math.Abs(rationalNumber2.Numeration / rationalNumber2.Denominator);
+        }
+
+        public static bool operator <=(RationalNumber rationalNumber1, RationalNumber rationalNumber2)
+        {
+            return (float)Math.Abs(rationalNumber1.Numeration / rationalNumber1.Denominator) >=
+                   (float)Math.Abs(rationalNumber2.Numeration / rationalNumber2.Denominator);
+        }
     }
 }

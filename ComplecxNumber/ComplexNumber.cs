131292cbc938230682823df307850d7c3332c1c1
IstominI <IStomin.I.A@Vnipipt.ru>
2021-11-11 09:21:40 +0300

    Add overide ToString. Correct method "*"

--- ComplecxNumber/ComplexNumber.cs
+++ ComplecxNumber/ComplexNumber.cs
@@ -1,9 +1,13 @@
-﻿namespace ComplecxNumber
+﻿using System;
+using System.Globalization;
+
+namespace ComplecxNumber
 {
     class ComplexNumber
     {
         private double _realPart;
         private double _imaginaryPart;
+        private bool _complex;
 
         public double RealPart
         {
@@ -17,6 +21,11 @@
             set => _imaginaryPart = value;
         }
 
+        public bool isComplex
+        {
+            get => _complex;
+        }
+
         private ComplexNumber()
         {
             
@@ -26,6 +35,7 @@
         {
             _realPart = realNumber;
             _imaginaryPart = imaginaryPart;
+            _complex = true;
         }
 
         public static ComplexNumber operator +(ComplexNumber number1, ComplexNumber number2)
@@ -48,11 +58,39 @@
 
         public static ComplexNumber operator *(ComplexNumber number1, ComplexNumber number2)
         {
+            if (Math.Abs(number1._realPart - number2._realPart) < 0.001 && Math.Abs(number1._imaginaryPart - (number2._imaginaryPart) * (-1)) < 0.001)
+            {
+                return new ComplexNumber()
+                {
+                    RealPart = number1.RealPart * number2.RealPart + (number1.ImaginaryPart * number2.ImaginaryPart) * (-1),
+                    ImaginaryPart = number1.ImaginaryPart * number2.RealPart + number1.RealPart * number2.ImaginaryPart,
+                    _complex = false
+                };
+            }
+
             return new ComplexNumber()
             {
-                RealPart = number1.RealPart * number2.RealPart + (number1.ImaginaryPart * number2.ImaginaryPart)*(-1),
-                ImaginaryPart = number1.ImaginaryPart * number2.RealPart + number1.RealPart * number2.ImaginaryPart
+                RealPart = number1.RealPart * number2.RealPart + (number1.ImaginaryPart * number2.ImaginaryPart) * (-1),
+                ImaginaryPart = number1.ImaginaryPart * number2.RealPart + number1.RealPart * number2.ImaginaryPart,
+                _complex = true
             };
+
+        }
+
+        public override string ToString()
+        {
+            var result = _realPart.ToString(CultureInfo.InvariantCulture);
+
+            if (_imaginaryPart > 0)
+            {
+                result += $"+{_imaginaryPart}i";
+            }
+            else
+            {
+                result += $"{_imaginaryPart}i";
+            }
+
+            return result;
         }
     }
 }

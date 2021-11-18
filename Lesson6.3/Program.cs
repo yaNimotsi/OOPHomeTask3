bec0619f4a3da73f2559c35014ff7d8df76c9b42
Иван Истомин <nimotsiky@gmail.com>
2021-11-18 19:37:38 +0300

    First commit. Clear project

---
+++ Lesson6.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

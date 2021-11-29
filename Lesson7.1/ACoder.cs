6bf44b13e4e6f53c6ede7c1854b15ca143c0c6e5
Иван Истомин <nimotsiky@gmail.com>
2021-11-29 21:53:21 +0300

    1

--- Lesson7.1/ACoder.cs
+++ Lesson7.1/ACoder.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lesson7._1
+﻿namespace Lesson7._1
 {
-    class ACoder: ICoder
     class ACoder:ICoder 
     {
         public string Encoder(string val)

b9671d78014d262ab646ff399dcfe95d0a2adaed
Иван Истомин <nimotsiky@gmail.com>
2021-11-29 21:49:40 +0300

    1

---
+++ Lesson7.1/ACoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7._1
+{
+    class ACoder:ICoder 
+    {
+        public string Encode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lesson7.1/ICoder.cs
@@ -0,0 +1,8 @@
+﻿namespace Lesson7._1
+{
+    interface ICoder
+    {
+        public string Encode();
+        public string Decode();
+    }
+}
